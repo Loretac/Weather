172686fd485d1aca32cc865eca33d41b9e26812b
Chris Loreta <LC@vt.edu>
2018-07-12 15:26:39 -0400

    now works for cities outside US, background changes based on weather/time of day, changed base font

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function bindButtons(){
             req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + payload.input + ",us&units=imperial&appid=" + apiKey, true);
         }
         else{
-            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + payload.input + ",us&units=imperial&appid=" + apiKey, true);
+            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + payload.input + "&units=imperial&appid=" + apiKey, true);
         }
 
 
@@ -58,4 +58,50 @@ function getInfo(responseText){
     document.getElementById("temp").textContent = Math.round(response.main.temp) + "\u00B0F";
     document.getElementById("hilo").textContent = "H " + Math.round(response.main.temp_max) + "\u00B0 / L " + Math.round(response.main.temp_min) + "\u00B0";
     document.getElementById("icon").src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+
+    document.body.style.transition = "all 2s";
+
+    var code = response.weather[0].icon;
+
+    if(code == "01d"){
+        document.body.style.backgroundColor = "#00BFFF"; // clear blue sky
+        document.body.style.color = "white";
+    }
+    else if(code == "02d"){
+        document.body.style.backgroundColor = "#99e6ff"; // day with some clouds
+        document.body.style.color = "black";
+    }
+    else if(code == "03d"){
+        document.body.style.backgroundColor = "#e6f0ff"; // cloudy day
+        document.body.style.color = "black";
+    }
+    else if(code == "04d"){
+        document.body.style.backgroundColor = "#cccccc"; // broken clouds
+        document.body.style.color = "black";
+    }
+    else if(code == "09d" || code == "10d" || code == "11d"){
+        document.body.style.backgroundColor = "#666666"; // rain or thunderstorm
+        document.body.style.color = "black";
+    }
+    else if(code == "13d"){
+        document.body.style.backgroundColor = "#e6f2ff"; // day snow
+        document.body.style.color = "black";
+    }
+    else if(code == "13n"){
+        document.body.style.backgroundColor = "#666699"; // night snow
+        document.body.style.color = "black";
+    }
+    else if(code == "50d"){
+        document.body.style.backgroundColor = "#e0e0eb"; // day mist
+        document.body.style.color = "black";
+    }
+    else if(code == "01n" || code == "02n"){
+        document.body.style.backgroundColor = "#28283e"; // night clear sky
+        document.body.style.color = "white";
+    }
+    else{
+        document.body.style.backgroundColor = "#262626"; // night other counditions
+        document.body.style.color = "white";
+    }
+
 }
