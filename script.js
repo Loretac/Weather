f43b7bcfd2ceb35852b05041f0fb04f53242fcc6
Chris Loreta <LC@vt.edu>
2018-07-12 22:04:55 -0400

    using JSONP for cross-domain. should now be functional on github pages

--- script.js
+++ script.js
@@ -9,47 +9,37 @@ function bindButtons(){
 
     // add event to the input submit button
     document.getElementById("submit").addEventListener("click", function(event){
-        var req = new XMLHttpRequest();
+        
 
-        // initialize input to null
+        var script = document.createElement('script');
+
         var payload = {input:null};
 
-        // get zip code from text box
+        // get input from text box
         payload.input = document.getElementById("userInput").value;
 
-        if(!isNaN(payload.input)){
-                    // since default value is kelvins, add &units=imperial
-            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + payload.input + ",us&units=imperial&appid=" + apiKey, true);
+        // check if input is a number
+        if(!isNaN(payload.input)){ // city name
+            script.src = "http://api.openweathermap.org/data/2.5/weather?zip=" + payload.input + ",us&units=imperial&appid=" + apiKey + "&callback=getInfo";
         }
-        else{
-            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + payload.input + "&units=imperial&appid=" + apiKey, true);
+        else{ // zip code
+            script.src = "http://api.openweathermap.org/data/2.5/weather?q=" + payload.input + "&units=imperial&appid=" + apiKey + "&callback=getInfo";
         }
 
+        document.getElementsByTagName('head')[0].appendChild(script);
 
-
-        // asynchronous request
-        req.addEventListener("load", function(){
-            if(req.status >= 200 && req.status < 400){
-                // retrieve info
-                getInfo(req.responseText);
-            }
-            else{
-                console.log("Error in network request: " + req.statusText);
-            }
-        });
-
-        // send the request
-        req.send(payload.input);
-
-        // prevent submit button from refreshing page
+        //prevent submit button from refreshing page
         event.preventDefault();
     });
 }
 
+
 // populate html spans with responseText
 function getInfo(responseText){
 
-    var response = JSON.parse(responseText);
+
+    //var response = JSON.parse(responseText);
+    var response = responseText;
 
     console.log(response);
 
@@ -59,8 +49,6 @@ function getInfo(responseText){
     document.getElementById("hilo").textContent = "H " + Math.round(response.main.temp_max) + "\u00B0 / L " + Math.round(response.main.temp_min) + "\u00B0";
     document.getElementById("icon").src = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
 
-    //document.body.style.transition = "all 2s";
-    //document.getElementById("container").style.transition = "all 2s";
 
     var code = response.weather[0].icon;
 
